71e33a0e5657d085c5d40fe736211c359e4a0cd0
HSR <hectorsilvarobles@gmail.com>
2019-04-29 15:33:44 -0700

    index.js

---
+++ Part1/index.js
@@ -0,0 +1,118 @@
+console.log("hello");
+
+const board = [];
+
+function play(clickedId) {
+  const playerSpan = document.getElementById("player");
+  const clickedElement = document.getElementById(clickedId);
+
+  if (playerSpan.innerText === "X") {
+    playerSpan.innerText = "O";
+    clickedElement.innerText = "X";
+    board[clickedId] = "X";
+  } else {
+    playerSpan.innerText = "X";
+    clickedElement.innerText = "O";
+    board[clickedId] = "O";
+  }
+
+  console.log(clickedElement);
+
+  const topLeft = board[0];
+  const topMiddle = board[1];
+  const topRight = board[2];
+  const middleLeft = board[3];
+  const middleMiddle = board[4];
+  const middleRight = board[5];
+  const bottomLeft = board[6];
+  const bottomMiddle = board[7];
+  const bottomRight = board[8];
+
+  if (topLeft !== undefined && 
+    topLeft === topMiddle && 
+    topLeft === topRight
+    ) {
+    alert(`${topLeft} is the winner!`);
+    return;
+  }
+  if (
+    middleLeft !== undefined &&
+    middleLeft === middleMiddle &&
+    middleLeft === middleRight
+  ) {
+    alert(`${middleLeft} is the winner!`);
+    return;
+  }
+  if (
+    bottomLeft !== undefined &&
+    bottomLeft === bottomMiddle &&
+    bottomLeft === bottomRight
+  ) {
+    alert(`${bottomLeft} is the winner!`);
+    return;
+  }
+  if (
+    topLeft !== undefined &&
+    topLeft === middleLeft &&
+    topLeft === bottomLeft
+  ) {
+    alert(`${topLeft} is the winner!`);
+    return;
+  }
+  if (
+    topMiddle !== undefined &&
+    topMiddle === middleMiddle &&
+    topMiddle === bottomMiddle
+  ) {
+    alert(`${topMiddle} is the winner!`);
+    return;
+  }
+  if (
+    topRight !== undefined &&
+    topRight === middleRight &&
+    topMiddle === bottomRight
+  ) {
+    alert(`${topRight} is the winner!`);
+    return;
+  }
+  if (
+    topRight !== undefined &&
+    topRight === middleMiddle &&
+    topRight === bottomLeft
+  ) {
+    alert(`${topRight} is the winner!`);
+    return;
+  }
+  if (
+    topLeft !== undefined &&
+    topLeft === middleMiddle &&
+    topLeft === bottomRight
+  ) {
+    alert(`${topLeft} is the winner!`);
+    return;
+  }
+
+  let boardFull = true;
+
+  for (i = 0; i <= 8; i++) {
+    if (board[i] === undefined) {
+      boardFull = false;
+    }
+  }
+
+  if (boardFull === true) {
+    alert("Cat's game, there is no winner");
+  }
+}
+
+function reset(){
+    board=[];
+    console.log(board);
+    const resetTags = document.getElementsByTagName("td");
+    for(let i = 0; i < resetTags.length; i++){
+        resetTags[i].innerText = '';
+    }
+}
+
+
+
